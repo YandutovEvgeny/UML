33431e0719fc9903586cabb885b60e5693972486
yandutka@mail.ru <yandutka@mail.ru>
2022-01-28 10:05:34 +0300

    Engine DONE

--- Car/Car.cpp
+++ Car/Car.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 #define MIN_TANK_VOLUME 40
 #define MAX_TANK_VOLUME 80
+#define MIN_ENGIN_CONSUMPTION 4
+#define MAX_ENGIN_CONSUMPTION 25
 
 class Tank
 {
@@ -47,9 +49,64 @@ public:
 	}
 };
 
+class Engine
+{
+	double consumption;
+	double consumption_per_second;
+	bool is_started;
+public:
+	double get_consumption()const
+	{
+		return consumption;
+	}
+	double get_consumption_per_second()const
+	{
+		return consumption_per_second;
+	}
+	bool started()const
+	{
+		return is_started;
+	}
+	bool start()
+	{
+		is_started = true;
+	}
+	bool stop()
+	{
+		is_started = false;
+	}
+	void set_consumption(double consumption)
+	{
+		if (consumption >= MIN_ENGIN_CONSUMPTION && consumption <= MAX_ENGIN_CONSUMPTION)
+			this->consumption = consumption;
+		else
+			this->consumption = MAX_ENGIN_CONSUMPTION / 2;
+		consumption_per_second = consumption * .3e-4;
+	}
+	explicit Engine(double consumption)
+	{
+		set_consumption(consumption);
+		is_started = false;
+		cout << "Engine is ready:\t" << this << endl;
+	}
+	~Engine()
+	{
+		cout << "Engine is gone:\t" << this << endl;
+	}
+	void info()const
+	{
+		cout << "Consumption:\t" << consumption << endl;
+		cout << "Consumption per second:\t" << consumption_per_second << endl;
+		cout << "Engine is " << (is_started ? "started" : "stopped") << endl;
+	}
+};
+
+//#define TANK_CHECK
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef TANK_CHECK
 	Tank tank(120);
 	tank.info();
 	while (true)
@@ -59,4 +116,8 @@ void main()
 		tank.fill(fuel);
 		tank.info();
 	}
+#endif // TANK_CHECK
+
+	Engine engine(9);
+	engine.info();
 }
