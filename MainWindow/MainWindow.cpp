8e8d142fe83dc1f37cf42ec2ee0ad9087b04b382
yandutka@mail.ru <yandutka@mail.ru>
2022-02-11 11:21:29 +0300

    UML DONE

--- MainWindow/MainWindow.cpp
+++ MainWindow/MainWindow.cpp
@@ -1,6 +1,7 @@
 //MainWindow
 #include<Windows.h>
 #include<stdio.h>
+#include"resource.h"
 
 CONST CHAR g_szCLASS_NAME[] = "MyWindowClass";
 CONST CHAR g_szTitle[] = "MyFirstTitleWindow";
@@ -17,9 +18,11 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	//wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_GLOBAL));  //Иконка в панели задач
+	//wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_GLOBAL)); //Small icon(Заголовок окна)
+	wc.hIcon = (HICON)LoadImage(hInstance, "Icon1.ico", IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
+	wc.hIconSm = (HICON)LoadImage(hInstance, "ConnectGlobe.ico", IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
+	wc.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR1));
 	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	wc.lpszClassName = g_szCLASS_NAME;		//g_sz - global string zero
 	wc.lpszMenuName = NULL;
